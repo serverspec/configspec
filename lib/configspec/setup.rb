fee7c6a4fce94d82f34c01a28d3d8e7d42f52f3f
Gosuke Miyashita <gosukenator@gmail.com>
2013-11-25 23:20:30 +0900

    Remove unsupported resources

--- lib/configspec/setup.rb
+++ lib/configspec/setup.rb
@@ -98,20 +98,6 @@ require 'spec_helper'
 describe package('httpd') do
   it { should be_installed }
 end
-
-describe service('httpd') do
-  it { should be_enabled   }
-  it { should be_running   }
-end
-
-describe port(80) do
-  it { should be_listening }
-end
-
-describe file('/etc/httpd/conf/httpd.conf') do
-  it { should be_file }
-  it { should contain "ServerName #{@hostname}" }
-end
 EOF
 
       if File.exists? "spec/#{@hostname}/001_httpd_spec.rb"
